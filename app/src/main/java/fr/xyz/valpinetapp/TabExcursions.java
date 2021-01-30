1ea1880e4a5ed242d2a06ddbac6166edbf1cf28f
Antoine CHAMPAULT <antoine.champault@hotmail.com>
2021-01-30 14:26:24 +0100

    CARTE MISE

--- app/src/main/java/fr/xyz/valpinetapp/TabExcursions.java
+++ app/src/main/java/fr/xyz/valpinetapp/TabExcursions.java
@@ -1,20 +1,25 @@
 package fr.xyz.valpinetapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class TabExcursions extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tab_excursions);
+    }
+
+    public void Back(View v){
+        finish(); //Retour à l'accueil.
     }
 
     public void onClik(View v){
-        Intent intent= new Intent(this, TabExcursions.class);
+        Intent intent= new Intent(this, Carte.class);
         startActivity(intent);
     }
 }
