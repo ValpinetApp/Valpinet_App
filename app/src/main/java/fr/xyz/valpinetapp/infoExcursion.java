f112246e23ed8c1e737fb2400d1d6498cf534adf
thomasxyz429 <tgcyberdragon@gmail.com>
2021-02-01 17:45:27 +0100

    Ajout esthetique precaire + pages
    -ajout du logo
    - ajout de la couleur du logo
    -ajout tableau statique
    -ajout page d'info
    -suppression des boutons carte et retour dans la page tabExcursion

---
+++ app/src/main/java/fr/xyz/valpinetapp/infoExcursion.java
@@ -0,0 +1,14 @@
+package fr.xyz.valpinetapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class infoExcursion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_excursion);
+    }
+}
