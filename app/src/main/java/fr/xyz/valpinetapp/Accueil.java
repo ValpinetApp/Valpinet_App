d23b03aaa1c31a3bbebe30058c18d282d5fa716b
Antoine CHAMPAULT <antoine.champault@hotmail.com>
2021-02-02 18:21:50 +0100

    sah

--- app/src/main/java/fr/xyz/valpinetapp/Accueil.java
+++ app/src/main/java/fr/xyz/valpinetapp/Accueil.java
@@ -6,12 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 
 
-public class MainActivity extends AppCompatActivity {
+public class Accueil extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_accueil);
     }
 
     public void onClik(View v){
