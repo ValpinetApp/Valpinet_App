4573c1de12cdd126e600eeef7d018bdb5e434ac0
Antoine CHAMPAULT <antoine.champault@hotmail.com>
2021-02-06 14:15:46 +0100

    Implémentation géoloc

--- app/src/main/java/fr/xyz/valpinetapp/Carte.java
+++ app/src/main/java/fr/xyz/valpinetapp/Carte.java
@@ -22,6 +22,14 @@ import android.util.Log;
 
 public class Carte extends AppCompatActivity implements OnMapReadyCallback  {
 
+    boolean mLocationPermissionGranted;
+    GoogleMap mMap; //Objet GoogleMap pour manipuler la carte (marqueurs etc)
+    private Location mLastKnownLocation; //Dernière position connue
+    private FusedLocationProviderClient mFusedLocationProviderClient; //Fournisseur de Loc
+    private static final int DEFAULT_ZOOM = 15; //Zoom carte par défaut
+    private static final String TAG = Carte.class.getSimpleName();
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,13 +40,106 @@ public class Carte extends AppCompatActivity implements OnMapReadyCallback  {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // Construction de FusedLocationProviderClient.
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(0, 0))
-                .title("Marker"));
-        }
+    public void onMapReady(GoogleMap map) {
+        mMap = map;
 
+        // Activités à faire pour tester, ici, position du marker à Sydney
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        map.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Vérif localisation activer
+        updateLocationUI();
+
+        // Obtention géolocalisation
+        getDeviceLocation();
+    }
+
+    private void getDeviceLocation() {
+        /*
+         * Obtention de la meilleure et la plus récente localisation de l'appareil, qui peut être nulle dans les rares cas où une localisation n'est pas disponible.
+         */
+        try {
+            if (mLocationPermissionGranted) {
+                Task locationResult = mFusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            // On positionne la caméra sur la position récupérée
+                            mLastKnownLocation = (Location) task.getResult();
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(mLastKnownLocation.getLatitude(),
+                                            mLastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                        } else {
+                            Log.d(TAG, "Current location is null. Using defaults.");
+                            Log.e(TAG, "Exception: %s", task.getException());
+                            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+        } catch(SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+    private void updateLocationUI() {
+        if (mMap == null) {
+            return;
+        }
+        try {
+            if (mLocationPermissionGranted) {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                mMap.setMyLocationEnabled(false);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                mLastKnownLocation = null;
+                getLocationPermission();
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+    private void getLocationPermission() {
+        /*
+         Demander l'autorisation de localisation, afin que nous puissions obtenir l'emplacement de l'appareil.
+          *Le résultat de la demande d'autorisation est traité par un rappel, onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+        updateLocationUI();
+    }
 }
+
