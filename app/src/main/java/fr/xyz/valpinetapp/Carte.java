89b44d8a25cab1f2c7b72a035347246768fcdb4f
Antoine CHAMPAULT <antoine.champault@hotmail.com>
2021-02-20 19:14:25 +0100

    ...

--- app/src/main/java/fr/xyz/valpinetapp/Carte.java
+++ app/src/main/java/fr/xyz/valpinetapp/Carte.java
@@ -1,26 +1,34 @@
 package fr.xyz.valpinetapp;
 
-import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.os.Bundle;
-import android.util.Log;
 
-public class Carte extends AppCompatActivity implements OnMapReadyCallback  {
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Carte extends AppCompatActivity implements OnMapReadyCallback {
+
+    boolean mLocationPermissionGranted;
+    GoogleMap mMap; //Objet GoogleMap pour manipuler la carte (marqueurs etc)
+    private Location mLastKnownLocation; //Dernière position connue
+    private FusedLocationProviderClient mFusedLocationProviderClient; //Fournisseur de Loc
+    private final LatLng mDefaultLocation = new LatLng(-33.8523341, 151.2106085); //Location par défaut
+    private static final int DEFAULT_ZOOM = 15; //Zoom carte par défaut
+    private static final String TAG = Carte.class.getSimpleName();
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +42,43 @@ public class Carte extends AppCompatActivity implements OnMapReadyCallback  {
         mapFragment.getMapAsync(this);
     }
 
-    @Override
     public void onMapReady(GoogleMap googleMap) {
-        googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(0, 0))
-                .title("Marker"));
+        mMap = googleMap;}
+
+
+    public void onClik(View v) {
+        createGpsDisabledAlert();
+    }
+
+
+    private void showGpsOptions() {
+        startActivityForResult(new Intent("android.settings.LOCATION_SOURCE_SETTINGS"), -1);
+    }
+
+
+    private void createGpsDisabledAlert() {
+        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE); //on recupere l etat du gps pour savoir si il est actif ou non
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            AlertDialog.Builder localBuilder = new AlertDialog.Builder(this);
+            Button fra = findViewById(R.id.button2);
+            localBuilder
+                    .setMessage("Le GPS est inactif, voulez-vous l'activer ?")
+                    .setCancelable(false)
+                    .setPositiveButton("Activer GPS ",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                                    mLocationPermissionGranted = true;
+                                    Carte.this.showGpsOptions();
+                                }
+                            });
+            localBuilder.setNegativeButton("Ne pas l'activer ",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                            paramDialogInterface.cancel();
+                        }
+                    });
+            localBuilder.create().show();
         }
+    }
 
 }
