91f79a5cbdd4c965972a210671e5ef29455bd3c7
Antoine CHAMPAULT <antoine.champault@hotmail.com>
2021-03-12 19:20:12 +0100

    GPS et nommage

--- app/src/main/java/fr/xyz/valpinetapp/Carte.java
+++ app/src/main/java/fr/xyz/valpinetapp/Carte.java
@@ -4,23 +4,18 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceManager;
 import android.content.Context;
+
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.Overlay;
 import org.osmdroid.views.overlay.ScaleBarOverlay;
 import org.osmdroid.views.overlay.compass.CompassOverlay;
 import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
@@ -32,7 +27,9 @@ import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 import java.util.ArrayList;
 
 public class Carte extends AppCompatActivity {
-    MapView map = null;
+    private org.osmdroid.views.MapView map = null;
+    private LocationManager manager;
+    private boolean estActif;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,10 +46,10 @@ public class Carte extends AppCompatActivity {
         //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's tile servers will get you banned based on this string
         //inflate and create the map
         setContentView(R.layout.activity_carte);
-        map = (MapView) findViewById(R.id.map);
+        map = (org.osmdroid.views.MapView) findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
 
-        map.setBuiltInZoomControls(true);
+
         map.setMultiTouchControls(true);
 
         IMapController mapController = map.getController();
@@ -60,13 +57,23 @@ public class Carte extends AppCompatActivity {
         GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
         mapController.setCenter(startPoint);
 
-
-        //creation de l'overlay personne
         MyLocationNewOverlay mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(this),map);
-        //activation de la recherche
-        mLocationOverlay.enableMyLocation();
-        //implementation sur la carte
-        map.getOverlays().add(mLocationOverlay);
+
+        manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE ); //on recupere l etat du gps pour savoir si il est actif ou non
+
+        if (!manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) )
+        {
+            createGpsDisabledAlert();
+        }
+        else{estActif = true;}
+
+        if(estActif==true){
+            //creation de l'overlay personne
+            //activation de la recherche
+            mLocationOverlay.enableMyLocation();
+            //implementation sur la carte
+            map.getOverlays().add(mLocationOverlay);
+        }
 
 
         //creation de l'overlay boussole
@@ -118,6 +125,35 @@ public class Carte extends AppCompatActivity {
         //Configuration.getInstance().save(this, prefs);
         map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
     }
+
+    private void createGpsDisabledAlert() {
+        AlertDialog.Builder localBuilder = new AlertDialog.Builder(this);
+        localBuilder
+                .setMessage("Le GPS est inactif, voulez-vous l'activer ?")
+                .setCancelable(false)
+                .setPositiveButton("Activer GPS ",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                                estActif = true;
+                                Carte.this.showGpsOptions();
+                            }
+                        }
+                );
+        localBuilder.setNegativeButton("Ne pas l'activer ",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        estActif=false;
+                        paramDialogInterface.cancel();
+                    }
+                }
+        );
+        localBuilder.create().show();
+    }
+
+    private void showGpsOptions() {
+        startActivityForResult(new Intent("android.settings.LOCATION_SOURCE_SETTINGS"),-1);
+    }
+
 
 }
 
